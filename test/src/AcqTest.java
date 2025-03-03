cb8197483ee5b7414a3c9c14cfc82eef3f299a67
Miloš Jovanović <milos@tehnocad.rs>
2025-03-03 10:47:42 +0100

    Basic acquisition test in java to verify MMCoreJ

---
+++ test/src/AcqTest.java
@@ -0,0 +1,77 @@
+import mmcorej.CMMCore;
+import mmcorej.TaggedImage;
+
+public class AcqTest {
+    public static void main(String[] args) {
+        // instantiate MMCore
+        CMMCore core = new CMMCore();
+
+        // decide how are we going to call our devices within this script
+        String store = "Store";
+        String camera = "Camera";
+
+        try {
+            // enable verbose logging
+            core.enableStderrLog(true);
+            core.enableDebugLog(true);
+
+            // load the demo camera device
+           core.loadDevice(camera, "DemoCamera", "DCam");
+
+            // initialize the system, this will in turn initialize each device
+            core.initializeAllDevices();
+
+            // configure the camera device
+            core.setProperty(camera, "PixelType", "16bit");
+            core.setProperty(camera, "OnCameraCCDXSize", "4432");
+            core.setProperty(camera, "OnCameraCCDYSize", "2368");
+            core.setExposure(5.0);
+
+            core.setCircularBufferMemoryFootprint(8192);
+            core.clearCircularBuffer();
+
+            // take one image to "warm up" the camera and get actual image dimensions
+            core.snapImage();
+            int w = (int)core.getImageWidth();
+            int h = (int)core.getImageHeight();
+
+            // fetch the image with metadata
+            TaggedImage img = core.getTaggedImage();
+            System.out.printf("Image %d X %d, size %d", w, h, ((short[])(img.pix)).length);
+
+            // print the metadata provided by MMCore
+            System.out.println(img.tags.toString());
+
+            int imgind = 0;
+            int imgcount = 16;
+            core.logMessage("START OF ACQUISITION");
+            core.startSequenceAcquisition(imgcount, 0.0, true);
+
+            for(int i = 0; i < imgcount; i++) {
+                // fetch the image
+                img = core.popNextTaggedImage();
+
+                // Add image index to the image metadata
+                img.tags.put("Image-index", imgind);
+
+                // add image to stream
+                short[] bx = (short[]) img.pix;
+
+                imgind++;
+                if (core.isBufferOverflowed())
+                    break;
+            }
+            long endAcq = System.nanoTime();
+            core.stopSequenceAcquisition();
+
+            core.logMessage("END OF ACQUISITION");
+            long end = System.nanoTime();
+
+            // unload all devices (not really necessary)
+            core.unloadAllDevices();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
